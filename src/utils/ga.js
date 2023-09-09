3bd2084e074821d6b4d399ad9b3f65e41432d272
BrettThurs10 <brettmarcthurston@gmail.com>
2023-09-09 05:51:08 -0500

    update google abalytics id used in config

--- src/utils/ga.js
+++ src/utils/ga.js
@@ -1,11 +1,7 @@
-import ReactGA from "react-ga";
-
 const googleAnalyticsID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
-export const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID;
-
 export const pageview = (url) => {
-  window.gtag("config", GA_MEASUREMENT_ID, {
+  window.gtag("config", googleAnalyticsID, {
     page_path: url,
   });
 };
